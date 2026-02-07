f831c35bd5effa68949b25ae65f05463b6b1788f
Fatima Jimoh <fatimaoyiza18@gmail.com>
2026-02-07 01:55:16 +0100

    Fetch data from backend using axios

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,8 +1,16 @@
+import { useEffect, useState } from 'react';
 import { Header } from '../components/Header';
-import { products } from '../data/products';
 import './HomePage.css';
+import axios from 'axios';
 
 const HomePage = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    axios.get('http://localhost:3000/api/products').then((response) => {
+      setProducts(response.data);
+    });
+  }, []);
+
   return (
     <>
       <link
