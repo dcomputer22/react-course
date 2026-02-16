04b658cbca7486ae5acb5942d408161b092af392
Fatima Jimoh <fatimaoyiza18@gmail.com>
2026-02-16 02:09:23 +0100

    Separate into smaller components

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from 'react';
 import { Route, Routes } from 'react-router';
-import HomePage from './pages/HomePage';
-import CheckoutPage from './pages/CheckoutPage';
-import OrdersPage from './pages/OrdersPage';
+import HomePage from './pages/home/HomePage';
+import CheckoutPage from './pages/checkout/CheckoutPage';
+import OrdersPage from './pages/orders/OrdersPage';
 import './App.css';
 import TrackingPage from './pages/TrackingPage';
 import NotFoundPage from './pages/NotFoundPage';
@@ -21,7 +21,7 @@ function App() {
     <Routes>
       <Route index element={<HomePage cart={cart} />} />
       <Route path="checkout" element={<CheckoutPage cart={cart} />} />
-      <Route path="orders" element={<OrdersPage />} />
+      <Route path="orders" element={<OrdersPage cart={cart} />} />
       <Route path="tracking" element={<TrackingPage />} />
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
---
+++ ecommerce-project/src/pages/checkout/CheckoutPage.jsx
@@ -0,0 +1,70 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router';
+import './CheckoutPage.css';
+import './checkout-header.css';
+import OrderSummary from './OrderSummary';
+import PaymentSummary from './PaymentSummary';
+
+const CheckoutPage = ({ cart }) => {
+  const [deliveryOptions, setDeliveryOptions] = useState([]);
+  const [paymentSummary, setPaymentSummary] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('api/delivery-options?expand=estimatedDeliveryTime')
+      .then((response) => {
+        setDeliveryOptions(response.data);
+      });
+
+    axios.get('api/payment-summary').then((res) => {
+      setPaymentSummary(res.data);
+    });
+  }, []);
+
+  return (
+    <>
+      <link
+        rel="icon"
+        type="image/svg+xml"
+        href="/images/favicons/cart-favicon.png"
+      />
+      <title>Checkout</title>
+
+      <div className="checkout-header">
+        <div className="header-content">
+          <div className="checkout-header-left-section">
+            <Link to="/">
+              <img className="logo" src="images/logo.png" />
+              <img className="mobile-logo" src="images/mobile-logo.png" />
+            </Link>
+          </div>
+
+          <div className="checkout-header-middle-section">
+            Checkout (
+            <Link className="return-to-home-link" to="/">
+              3 items
+            </Link>
+            )
+          </div>
+
+          <div className="checkout-header-right-section">
+            <img src="images/icons/checkout-lock-icon.png" />
+          </div>
+        </div>
+      </div>
+
+      <div className="checkout-page">
+        <div className="page-title">Review your order</div>
+
+        <div className="checkout-grid">
+          <OrderSummary deliveryOptions={deliveryOptions} cart={cart} />
+
+          <PaymentSummary paymentSummary={paymentSummary} />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CheckoutPage;
